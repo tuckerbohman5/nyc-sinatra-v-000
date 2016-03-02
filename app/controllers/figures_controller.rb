72c587e3a47fccea00dc84bbe38e6a9f4a9c78c4
tuckerbohman5 <tucker.bohman@gmail.com>
2016-03-02 11:21:27 -0700

    All figure controller tests passing

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,47 @@
 class FiguresController < ApplicationController
+  get '/figures' do 
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do 
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'figures/new'
+  end 
+
+  post '/figures' do 
+    @figure = Figure.create(params[:figure])
+    @figure.landmarks << Landmark.create(params[:landmark])
+    @figure.titles << Title.create(params[:title])
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+
+  end
+
+  get '/figures/:id' do 
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do 
+    @figure = Figure.find_by_id(params[:id])
+    @landmarks = Landmark.all
+    @titles = Title.all
+
+    erb :'figures/edit'
+  end
+
+  post '/figures/:id' do 
+    @figure = Figure.find_by_id(params[:id])
+    @figure.name = params[:figure][:name]
+    @figure.landmarks << Landmark.find_or_create_by(params[:landmark])
+    @figure.titles << Title.find_or_create_by(params[:title])
+    @figure.save
+
+    redirect "/figures/#{@figure.id}"
+
+  end
 
 end
