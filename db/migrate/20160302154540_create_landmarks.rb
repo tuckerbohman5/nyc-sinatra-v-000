b3ec51557c72a6e1862e2a81ede40ef8cab82210
tuckerbohman5 <tucker.bohman@gmail.com>
2016-03-02 09:04:17 -0700

    Created migrations and models/all model tests passing

---
+++ db/migrate/20160302154540_create_landmarks.rb
@@ -0,0 +1,12 @@
+class CreateLandmarks < ActiveRecord::Migration
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :figure_id
+      t.integer :year_completed
+
+      t.timestamps
+    end
+
+  end
+end
